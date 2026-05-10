6794bc5c16d0cc3e2a95d682a38c115278d3d605
ALEX HUNTER <cybercctv90@gmail.com>
2026-05-11 01:39:38 +0600

    Update set.py

--- set.py
+++ set.py
@@ -1,2 +1,10 @@
 import Tarmuxset
-Tarmuxset.auto_run()
+import inspect
+
+try:
+    print("Auto_run arguments:", inspect.signature(Tarmuxset.auto_run))
+except:
+    print("Signature not found. Trying main...")
+
+# যদি main কাজ করে তবে এটিই ব্যবহার করুন
+Tarmuxset.main()
